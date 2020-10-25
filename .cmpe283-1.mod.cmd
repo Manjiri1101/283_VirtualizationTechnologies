0598fc76435db2c252bcefe3b887dba95836fbf9
Manjiri1101 <manjiri1994@outlook.com>
2020-10-25 16:29:02 -0700

    Added my project

---
+++ .cmpe283-1.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/manjiri/CMPE283-1/cmpe283-1.mod := { echo  /home/manjiri/CMPE283-1/cmpe283-1.o; echo; } > /home/manjiri/CMPE283-1/cmpe283-1.mod
---
+++ cmpe283-1.mod
@@ -0,0 +1,2 @@
+/home/manjiri/CMPE283-1/cmpe283-1.o
+
